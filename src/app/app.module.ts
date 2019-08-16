c8515f882850edf8898353907004751c5804975d
JonyDMorales <jonatan_moralest@hotmail.com>
2019-08-16 15:11:57 -0500

    Paquete inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { OurservicesComponent } from './components/ourservices/ourservices.component';
+import { WeworkComponent } from './components/wework/wework.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    OurservicesComponent,
+    WeworkComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/wework/wework.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wework',
+  templateUrl: './wework.component.html',
+  styleUrls: ['./wework.component.css']
+})
+export class WeworkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
